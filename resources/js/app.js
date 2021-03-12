5ba740c532606f58aa1329f74ba8c6f0aa9c0825
RezaParsian <RezaParsian76@gmail.com>
2021-03-13 02:03:20 +0330

    Start new project for my self. i will make personal webpage as a blog add all style and js file make markdown editor

---
+++ resources/js/app.js
@@ -0,0 +1,34 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
+require('./bootstrap');
+require('./custom');
+require('./run_prettify');
+window.Vue = require('vue').default;
+/**
+ * The following block of code may be used to automatically register your
+ * Vue components. It will recursively scan this directory for the Vue
+ * components and automatically register them with their "basename".
+ *
+ * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
+ */
+
+// const files = require.context('./', true, /\.vue$/i)
+// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+
+Vue.component('markdown', require('./components/MarkDownEditor.vue').default);
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        lorm: "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.",
+    }
+});
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>
+        {{ env('APP_NAME') }}
+        @hasSection ('ex-title')
+            - @yield('ex-title')
+        @endif
+    </title>
+    <style>
+        kholase {
+            white-space: pre-wrap;
+        }
+
+        #markdown {
+            direction: rtl;
+            text-align: right;
+        }
+
+        .CodeMirror {
+            padding-right: 1rem !important;
+        }
+
+        .rtl{
+            text-align: right;
+            direction: rtl !important;
+        }
+
+        .ltr{
+            text-align: left;
+            direction: ltr !important;
+        }
+    </style>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+
+<body>
+    <div class="container" id="app">
+        <div id="markdown">
+            {!! $b !!}
+        </div>
+        <form action="">
+            <markdown></markdown>
+            <input type="submit" value="Go" id="btnform">
+        </form>
+    </div>
+    <script src="{{ asset('js/app.js') }}"></script>
+</html>
